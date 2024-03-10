18d765541f4349235ef39e6d73f0ebbd3b5032db
Ismaël <ismael.guerrib@hotmail.fr>
2024-03-10 15:56:11 +0100

    feat : protect potential critical value in .env

--- index.ts
+++ index.ts
@@ -1,14 +1,10 @@
-// https://bun.sh/
-
-const host = Bun.env.HOST;
-
 Bun.serve({
-  port: 36107,
+  port: Bun.env.PORT,
   fetch: async () => {
-    const res = await fetch(`https://${host}`);
+    const res = await fetch(Bun.env.HOST as string);
     const content = await res.text();
     return new Response(content);
   },
 });
 
-console.log('Run on http://localhost:36107');
+console.log('Run on', Bun.env.LOCAL_HOST);
