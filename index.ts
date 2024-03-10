038108d39d40aab396dcd5fbcc399152def23280
Ismaël <ismael.guerrib@hotmail.fr>
2024-03-10 15:05:27 +0100

    core: install bun

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 // https://bun.sh/
 
-const host = "www.dow.com";
+const host = 'www.dow.com';
 
 Bun.serve({
   port: 36107,
-  fetch: async (request) => {
+  fetch: async () => {
     const res = await fetch(`https://${host}`);
     const content = await res.text();
     return new Response(content);
   },
 });
 
-console.log("Run on http://localhost:36107");
+console.log('Run on http://localhost:36107');
